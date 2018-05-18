fb3b7dff0ea6f28ec53191a4cc0421c9a5ff23a0
Alex Z <ajz44@drexel.edu>
2018-05-17 22:03:24 -0400

    1. Edited get_coordinates.py to output data results to JSON file 2. Subsequently created a JSON file 3. Added homepage along with CSS

--- get_coordinates.py
+++ get_coordinates.py
@@ -15,18 +15,27 @@ def search_by_location(latitude, longitude, radius, number_of_tweets):
     num = int(number_of_tweets)
 
     tweets = tweepy.Cursor(api.search, geocode=lat+','+lon+','+dis+"km").items(num)
-
+    file = open('data.json', 'w')
+    # empty list to contain coordinates
+    l = []
     for tweet in tweets:
+        # make tweets json format
         data = json.dumps(tweet._json)
         parseable_data = json.loads(data)
         try:
-            print(parseable_data['geo']['coordinates'])
+            # add collected coordinates to list
+            l.append(parseable_data['geo']['coordinates'])
         except:
             pass
-
+    # converts list to json
+    list_to_json = json.loads(str(l))
+    # formats json so it's readable
+    json_list = json.dumps(list_to_json, indent=4)
+    # writes formatted json to file
+    file.write(json_list)
+    file.close()
 
 def search_by_query(search_term, number_of_tweets):
-    
     query = str(search_term)
     num = int(number_of_tweets)
     tweets = tweepy.Cursor(api.search, q=query).items(num)
